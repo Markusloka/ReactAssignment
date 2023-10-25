ca396496b5d87018989445643526734d2eec98ab
Markusloka <Markuslokander97@gmail.com>
2023-10-25 16:48:25 +0200

    Added the Navbar.

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import "./style.css";
+import CritoLogo from "../../assets/hero/LogoCrito.svg";
+import PhoneIcon from "../../assets/hero/Phone.svg";
+import EmailIcon from "../../assets/hero/Email.svg";
+import LocationIcon from "../../assets/hero/Location.svg";
+import ArrowUp from "../../assets/hero/ArrowUp Right.svg";
+function Navbar() {
+  return (
+    <div className="navbar">
+      <img src={CritoLogo} alt="Crito logo" width="137" height="57" />
+      <div className="contactInfo">
+        <div className="contacts">
+          <img src={PhoneIcon} />
+        </div>
+        <div className="contacts">
+          <img src={EmailIcon} />
+        </div>
+        <div className="contacts">
+          <img src={LocationIcon} />
+        </div>
+      </div>
+      <button className="loginButton">
+        Login <img src={ArrowUp} />
+      </button>
+    </div>
+  );
+}
+
+export default Navbar;
