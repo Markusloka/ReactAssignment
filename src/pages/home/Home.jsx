8adc2ade5f6bcedf3138b63d6c47cab777737786
Markusloka <Markuslokander97@gmail.com>
2023-10-26 16:25:55 +0200

    added logos to the home view

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 import Features from "./components/Home.features";
 import Hero from "./components/Home.hero";
+import Logos from "./components/Home.logos"
 
 function Home() {
   return (
     <Fragment>
       <Hero />
+      <Logos />
       <Features />
     </Fragment>
   );
