9730cc1e340333e40fcc3c44ecdf94815a768ddb
Markusloka <Markuslokander97@gmail.com>
2023-10-26 11:28:55 +0200

    Clean up, and switch to fragment > array

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,14 @@
+import { Fragment } from "react";
 import Features from "./components/Home.features";
 import Hero from "./components/Home.hero";
-import Logos from "./components/Home.logos";
 
 function Home() {
-  return [<Hero />, <Logos />, <Features />];
+  return (
+    <Fragment>
+      <Hero />
+      <Features />
+    </Fragment>
+  );
 }
 
 export default Home;
