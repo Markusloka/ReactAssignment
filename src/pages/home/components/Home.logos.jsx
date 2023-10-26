4df25e20caedd4f734d2cfb00cafffb951f7ed95
Markusloka <Markuslokander97@gmail.com>
2023-10-26 16:24:20 +0200

    update gitignore to support logos

---
+++ src/pages/home/components/Home.logos.jsx
@@ -0,0 +1,21 @@
+import "./style.css";
+import PaperzLogo from "../../../assets/logos/Paperz.svg";
+import DorfusLogo from "../../../assets/logos/DorfusLogo.svg";
+import MartinoLogo from "../../../assets/logos/MartinoLogo.svg";
+import Squarelogo from "../../../assets/logos/squarelogo.svg";
+import GobonaLogo from "../../../assets/logos/GobonaLogo.svg";
+
+function Logos(){
+return(
+    <div className="logos">
+      <img src={PaperzLogo} />
+      <img src={DorfusLogo} />
+      <img src= {MartinoLogo} />
+      <img src= {Squarelogo} />
+      <img src= {GobonaLogo} />
+    </div>
+
+)
+}
+
+export default Logos;
