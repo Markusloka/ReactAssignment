da710fa49fa4d485837bb8c078dd1ab9cf1179cd
Markusloka <Markuslokander97@gmail.com>
2023-11-01 15:19:07 +0100

    Added form validation and messages

--- src/pages/contact/components/Form.jsx
+++ src/pages/contact/components/Form.jsx
@@ -4,8 +4,9 @@ import ArrowUp from "../../../assets/hero/ArrowUp Right.svg";
 export function Form() {
   const {
     register,
-    handleSubmit,
+
     formState: { errors },
+    handleSubmit,
   } = useForm();
   console.log(errors);
   return (
@@ -15,24 +16,36 @@ export function Form() {
         console.log(data);
       })}
     >
-      <input
-        className="Name"
-        type="text"
-        {...register("Namn", { required: true, minLength: 2 })}
-        placeholder="Namn"
-      ></input>
-      <input
-        className="Email"
-        type="email"
-        {...register("Email", { required: true })}
-        placeholder="Epost-adress"
-      ></input>
-      <input
-        className="Message"
-        type="text"
-        {...register("Message", { required: true })}
-        placeholder="Skriv ditt meddelande"
-      ></input>
+      <div>
+        <input
+          className="namn"
+          placeholder="Name*"
+          {...register("firstName", {
+            required: "First name is requried",
+            minLength: 2,
+          })}
+          aria-invalid={errors.firstName ? "true" : "false"}
+        />
+        {errors.firstName && <p role="alert">{errors.firstName.message}</p>}
+      </div>
+      <div>
+        <input
+          className="email"
+          placeholder="Email*"
+          {...register("email", { required: "Need to fill a valid email" })}
+          aria-invalid={errors.email ? "true" : "false"}
+        />
+        {errors.email && <p role="alert">{errors.email.message}</p>}
+      </div>
+      <div>
+        <textarea
+          className="message"
+          placeholder="Your message*"
+          {...register("message", { required: "You need to fill a message" })}
+          aria-invalid={errors.message ? "true" : "false"}
+        />
+        {errors.message && <p role="alert">{errors.message.message}</p>}
+      </div>
 
       <button type="submit" className="sendButton">
         Send Message <img src={ArrowUp} />
